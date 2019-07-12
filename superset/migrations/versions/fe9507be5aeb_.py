296ebfd7636d602f42e79ab5b0b6dbe6a7f93571
Andrey Tvorozhkov <tvorog@tvorog.me>
2019-07-12 11:27:41 +0300

    Fix T00:00:00.000Z suffix in time filter

---
+++ superset/migrations/versions/fe9507be5aeb_.py
@@ -0,0 +1,38 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""empty message
+
+Revision ID: fe9507be5aeb
+Revises: ('b4a38aa87893', 'd6ffdf31bdd4')
+Create Date: 2019-07-11 22:18:54.371863
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'fe9507be5aeb'
+down_revision = ('b4a38aa87893', 'd6ffdf31bdd4')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
